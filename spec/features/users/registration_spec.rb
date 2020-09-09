061aa5283a91d8803f79e62f48876fcdf1750f23
Priya Power <priyajag72@gmail.com>
2020-09-09 17:49:10 -0600

    Add test for us12; registation email must be unique

--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "User Registration Page", type: :feature do
   describe "As a visitor" do
-    it "Can register a user" do
+    before :each do
+      @user = User.create(name:"Luke Hunter James-Erickson", address:"skdjfhdskjfh", city:"kajshd", state:"jsdh", zip:"88888", email: "tombroke@gmail.com", password:"Iamapassword", password_confirmation:"Iamapassword")
+    end
 
+    it "Can register a user" do
       visit "/merchants"
 
       within(".topnav") do
@@ -45,5 +48,26 @@ RSpec.describe "User Registration Page", type: :feature do
       expect(page).to have_content("You are missing one or more required fields")
     end
 
+    it "can require a registration email be unique" do
+
+      visit "/register"
+      username = "Tom Brokaw"
+      test_email = "tombroke@gmail.com"
+      fill_in :name, with: username
+      fill_in  :address, with: "131 Hills Ave"
+      fill_in  :city, with: "Tomville"
+      fill_in  :state, with: "CO"
+      fill_in  :zip, with: "82828"
+      fill_in  :email, with: test_email
+      fill_in  :password, with: "hiohio38298"
+      fill_in  :password_confirmation, with: "hiohio38298"
+
+      click_button "Sign-up"
+
+      expect(current_path).to eq("/register")
+      expect(page).to have_content("131 Hills Ave")
+      expect(page).to_not have_content(test_email)
+      expect(page).to have_content("The email address #{test_email} is already in use")
+    end
   end
 end
