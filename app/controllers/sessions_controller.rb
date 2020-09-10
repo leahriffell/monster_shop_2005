fd6350355a81d62b44a9bb2ac63fe7c757bcd3bc
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-10 11:58:57 -0600

    Add User Model Test for new attribute, role.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,12 @@
 class SessionsController < ApplicationController
 
   def new
-
   end
 
   def create
     user = User.find_by(email: params[:email])
     session[:user_id] = user.id
-    flash[:success] = "Welcome, #{user.name}. You are logged in."
+    flash[:success] = "Welcome, #{user.name}. You are logged in!"
     redirect_to "/profile"
   end
 end
--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe "Login Page", type: :feature do
   describe "As a visitor" do
     before :each do
-      @user = User.create(name:"Luke Hunter James-Erickson", address:"skdjfhdskjfh", city:"kajshd", state:"jsdh", zip:"88888", email: "tombroke@gmail.com", password:"Iamapassword", password_confirmation:"Iamapassword")
+      @user = User.create(name:"Luke Hunter James-Erickson", address:"skdjfhdskjfh", city:"kajshd", state:"jsdh", zip:"88888", email: "tombroke@gmail.com", password:"Iamapassword", password_confirmation:"Iamapassword", role: 0)
+
+      @merchant = User.create(name:"Leah", address:"123 Sesame Street", city:"New York", state:"NY", zip:"90210", email: "Leahsocool@gmail.com", password:"Imeanit", password_confirmation:"Imeanit", role: 1)
+
+      @admin = User.create(name:"Priya", address:"13 Elm Street", city:"Denver", state:"CO", zip:"66666", email: "priyavcooltoo@gmail.com", password:"yuuuuuup", password_confirmation:"yuuuuuup", role: 2)
 
       visit "/merchants"
 
@@ -24,38 +28,45 @@ RSpec.describe "Login Page", type: :feature do
 
       expect(current_path).to eq("/profile")
 
-      expect(page).to have_content("Welcome, #{@user.name}. You are logged in.")
+      expect(page).to have_content("Welcome, #{@user.name}. You are logged in!")
     end
 
 
 
 
-    xit "can login a merchant user" do
+    it "can login a merchant user" do
+
+
 
       expect(current_path).to eq("/login")
 
-      fill_in :email, with: @user.email
-      fill_in  :password, with: @user.password
+      fill_in :email, with: @merchant.email
+      fill_in  :password, with: @merchant.password
 
       click_button "Login"
 
-      expect(current_path).to eq("/  Merchant Dashboard ")
+      expect(current_path).to eq("/merchants/dashboard")
 
-      expect(page).to have_content("Welcome, #{@user.name}")
+      expect(page).to have_content("Welcome, #{@merchant.name}. You are logged in!")
     end
+
+
+
+
+
 
     xit "can login an admin user" do
 
       expect(current_path).to eq("/login")
 
-      fill_in :email, with: @user.email
-      fill_in  :password, with: @user.password
+      fill_in :email, with: @admin.email
+      fill_in  :password, with: @admin.password
 
       click_button "Login"
 
-      expect(current_path).to eq("/   Admin Dashboard")
+      expect(current_path).to eq("/admin/dashboard")
 
-      expect(page).to have_content("Welcome, #{@user.name}")
+      expect(page).to have_content("Welcome, #{@admin.name}. You are logged in!")
     end
 
 
