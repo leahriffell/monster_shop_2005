ecf9e4f2a194dd0ea53558f6458191766cd9101c
Leah Riffell <leah.riffell@gmail.com>
2020-09-09 19:40:08 -0600

    Change user to an instance variable in create action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,25 +6,21 @@ class UsersController < ApplicationController
   end
   
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       # condition 1 = all params are great
-      flash[:success] = "Welcome, #{user.name}"
+      flash[:success] = "Welcome, #{@user.name}"
       redirect_to '/profile'
-      # condition 2 = email param she empty
-    elsif user_params.values.any?('') == true
+    elsif user_params.values.any?('')
       # condition 1 - just missing field
       flash[:failure] = "You are missing one or more required fields"
-      redirect_to '/register'
+      render :new
     else
-      
-      session[:name] = "POOP"
-      require 'pry'; binding.pry
-      session[:address] = user_params[:address]
-      redirect_to '/register'
-      # render :new
+      # condition 2 = email param she empty
+      render :new
+      # session[:address] = user_params[:address]
       # pass params to a session and call from there
-      #within "new" we would have that logic (maybe a PORO?)
+      # within "new" we would have that logic (maybe a PORO?)
       # condition 2 - email isn't unique - save as @user without pass/email (model condition that allows this, private empty_fields)?
         # user.update(passowrd, email)
     end
