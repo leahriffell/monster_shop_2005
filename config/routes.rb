372c7fa186acbd794cd9ebc74e4d088a60db32ad
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-10 11:42:30 -0600

    Add SessionController, session new.html.erb, update routes to pass Login Test.

--- config/routes.rb
+++ config/routes.rb
@@ -37,4 +37,7 @@ Rails.application.routes.draw do
   resources :users, only: [:create]
   get "/register", to: "users#new"
   get "/profile", to: "users#show"
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
 end
