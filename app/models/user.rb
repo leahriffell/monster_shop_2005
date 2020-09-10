1177186f4174ac5f857f50b881a9c3e8df4b272e
Leah Riffell <leah.riffell@gmail.com>
2020-09-09 20:16:18 -0600

    Remove allowance for email to be blank in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
   validates_presence_of :name, :address, :city, :state, :zip, :password, :password_confirmation
 
-  validates :email, allow_blank: true, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
 
   # validates_presence_of :password, require: true
   has_secure_password
