5383e3828495ba056acb9faef7dd2de358909a5d
Priya Power <priyajag72@gmail.com>
2020-09-09 18:24:40 -0600

    Update user action#create with pseudocode for us12

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,19 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      # condition 1 = all params are great
       flash[:success] = "Welcome, #{user.name}"
       redirect_to '/profile'
-    else
+      # condition 2 = email param she empty
+    elsif
+      # condition 1 - just missing field
       flash[:failure] = "You are missing one or more required fields"
       redirect_to '/register'
+    else
+      # pass params to a session and call from there
+      #within "new" we would have that logic (maybe a PORO?)
+      # condition 2 - email isn't unique - save as @user without pass/email (model condition that allows this, private empty_fields)?
+        # user.update(passowrd, email)
     end
 
   end
