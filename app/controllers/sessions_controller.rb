9b0bf8fa93c8432409705e7b8c62a86124448462
Priya Power <priyajag72@gmail.com>
2020-09-11 07:20:53 -0600

    Update session#new for redirection by user role if already logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,17 @@
 class SessionsController < ApplicationController
 
   def new
+    if session[:user_id]
+      user = User.find(session[:user_id])
+      # Can we make this condition a model method (maybe a case statement)? Use it to refactor sessions#create as well?
+      if user.role == "admin"
+        redirect_to(admin_dashboard_path)
+      elsif user.role == "merchant"
+        redirect_to(merchants_dashboard_path)
+      else
+        redirect_to(profile_path)
+      end
+    end
   end
 
   def create
@@ -8,7 +19,7 @@ class SessionsController < ApplicationController
     if user == nil || !user.authenticate(params[:password])
       flash[:failure] = "Incorrect email or password."
       redirect_to(login_path)
-    else 
+    else
       session[:user_id] = user.id
       if user.role == "admin"
         flash[:success] = "Welcome Admin, #{user.name}!"
