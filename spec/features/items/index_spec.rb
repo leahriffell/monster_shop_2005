12698f76b29f9f70707357c5927649ddb7266ede
Priya Power <priyajag72@gmail.com>
2020-09-12 15:13:51 -0600

    Update items index spec to remove old conflicting tests

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -19,8 +19,7 @@ RSpec.describe "Items Index Page" do
       expect(page).to have_link(@tire.merchant.name)
       expect(page).to have_link(@pull_toy.name)
       expect(page).to have_link(@pull_toy.merchant.name)
-      expect(page).to have_link(@dog_bone.name)
-      expect(page).to have_link(@dog_bone.merchant.name)
+      expect(page).to_not have_link(@dog_bone.name)
     end
 
     it "I can see a list of all of the items "do
@@ -47,25 +46,12 @@ RSpec.describe "Items Index Page" do
         expect(page).to have_css("img[src*='#{@pull_toy.image}']")
       end
 
-      within "#item-#{@dog_bone.id}" do
-        expect(page).to_not have_link(@dog_bone.name)
-        expect(page).to_not have_content(@dog_bone.description)
-        expect(page).to_not have_content("Price: $#{@dog_bone.price}")
-        expect(page).to_not have_content("Inactive")
-        expect(page).to_not have_content("Inventory: #{@dog_bone.inventory}")
-        expect(page).to_not have_link(@brian.name)
-        expect(page).to_not have_css("img[src*='#{@dog_bone.image}']")
-      end
+      expect(page).to_not have_link(@dog_bone.name)
+      expect(page).to_not have_content(@dog_bone.description)
+      expect(page).to_not have_content("Price: $#{@dog_bone.price}")
+      expect(page).to_not have_content("Inactive")
+      expect(page).to_not have_content("Inventory: #{@dog_bone.inventory}")
+      expect(page).to_not have_css("img[src*='#{@dog_bone.image}']")
     end
-
-    # it "can display all active items" do
-    #   visit "/items"
-    #
-    #   expect(page).to have_content(@tire.name)
-    #   expect(page).to have_content(@pull_toy.name)
-    #   expect(page).to_not have_content(@dog_bone.name)
-    # end
-    #
-    # it "can display all active items with image links to show page"
   end
 end
