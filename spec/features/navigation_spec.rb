d73cdc3a270b7b0583d8dfb30b805b320cc73202
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-12 11:04:19 -0600

    Update US9 tests - adding admin login.

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -65,16 +65,14 @@ RSpec.describe 'Site Navigation' do
       expect(page).to have_content("The page you were looking for doesn't exist.")
     end
 
-    # it "can redirect an admin to 404 error if they try to access any path with /merchant" do
-    #   visit "/merchants"
-    #   expect(page).to have_content("The page you were looking for doesn't exist.")
-    # end
-    #
-    # it "can redirect an admin to 404 error if they try to access any path with /admin" do
-    #   visit "/cart"
-    #   save_and_open_page
-    #   expect(page).to have_content("The page you were looking for doesn't exist.")
-    # end
+    it "can redirect an admin to 404 error if they try to access any path with /merchant" do
+      fill_in :email, with: @admin.email
+      fill_in  :password, with: @admin.password
+      click_button "Login"
+
+      visit "/merchants"
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
 
     it "I can see a profile link if logged in on all pages" do
       fill_in :email, with: @user.email
