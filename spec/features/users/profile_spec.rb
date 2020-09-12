12f391aee122c0c210f54bcead771e789d8ae34c
Leah Riffell <leah.riffell@gmail.com>
2020-09-12 16:14:08 -0600

    Add test for user seeing their data on profile page

---
+++ spec/features/users/profile_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "user profile page", type: :feature do
+  describe "As a logged-in user" do
+
+    before :each do
+      @user = User.create(name:"Luke Hunter James-Erickson", address:"123 Lane", city:"Denver", state:"CO", zip:"88888", email: "tombroke@gmail.com", password:"Iamapassword", password_confirmation:"Iamapassword", role: 0)
+ 
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+      visit "/profile"
+    end
+
+    it "can see my profile data (except for password)" do
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@user.address)
+      expect(page).to have_content(@user.city)
+      expect(page).to have_content(@user.state)
+      expect(page).to have_content(@user.zip)
+      expect(page).to have_content(@user.email)
+      expect(page).to_not have_content(@user.password)
+      expect(page).to_not have_content(@user.password_confirmation)
+    end
+  end
+end
