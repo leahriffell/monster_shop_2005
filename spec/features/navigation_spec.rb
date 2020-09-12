87a7597f214afa687fbe5e47b727254e99d054b4
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-12 11:29:39 -0600

    Remove unnecessary lines from Spec test - US9

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -5,9 +5,7 @@ RSpec.describe 'Site Navigation' do
   describe 'As a Visitor' do
     before :each do
       @user = User.create(name:"Jackie Chan", address:"skdjfhdskjfh", city:"kajshd", state:"jsdh", zip:"88888", email: "tombroke@gmail.com", password:"Iamapassword", password_confirmation:"Iamapassword", role: 0)
-
       @merchant = User.create(name:"Leah", address:"123 Sesame Street", city:"New York", state:"NY", zip:"90210", email: "Leahsocool@gmail.com", password:"Imeanit", password_confirmation:"Imeanit", role: 1)
-
       @admin = User.create(name:"Priya", address:"13 Elm Street", city:"Denver", state:"CO", zip:"66666", email: "priyavcooltoo@gmail.com", password:"yuuuuuup", password_confirmation:"yuuuuuup", role: 2)
 
       visit '/merchants'
