88e52bccf1207406e58ee4699be324a9c8b590eb
Priya Power <priyajag72@gmail.com>
2020-09-12 16:19:08 -0600

    Add test to cart add_spec for a visitor updating quantities of items in cart

--- spec/features/cart/add_spec.rb
+++ spec/features/cart/add_spec.rb
@@ -2,35 +2,66 @@ require 'rails_helper'
 
 RSpec.describe 'Cart creation' do
   describe 'When I visit an items show page' do
-
     before(:each) do
       @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
       @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      @quantity_test_object_visitor = @mike.items.create(name: "sdfsd", description: "sdfsdfdsfsdf", price: 10, image: "https://previews.123rf.com/images/albertzig/albertzig1210/albertzig121001618/16005008-3d-cartoon-cute-monster.jpg", inventory: 2)
+      @quantity_test_object_regular = @mike.items.create(name: "vbnmnbmbnm", description: "ytuyuy", price: 10, image: "https://previews.123rf.com/images/albertzig/albertzig1210/albertzig121000361/15625219-3d-cartoon-cute-monster.jpg", inventory: 3)
+      @quantity_test_object_merchant = @mike.items.create(name: "yuiyui", description: "kjgsddsfhg", price: 10, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSSEiGA-5ciHpGWaYBQ-lD25g0mbpYDnhi8xw&usqp=CAU", inventory: 2)
+      @quantity_test_object_admin = @mike.items.create(name: "sdaddwwee", description: "6ythdrgfcsx", price: 10, image: "https://static.turbosquid.com/Preview/001299/713/NK/_D.jpg", inventory: 3)
     end
 
-    it "I see a link to add this item to my cart" do
-      visit "/items/#{@paper.id}"
-      expect(page).to have_button("Add To Cart")
+    describe "As a visitor" do
+      it "I see a link to add this item to my cart" do
+        visit "/items/#{@paper.id}"
+        expect(page).to have_button("Add To Cart")
+      end
+
+      it "I can add this item to my cart" do
+        visit "/items/#{@paper.id}"
+        click_on "Add To Cart"
+
+        expect(page).to have_content("#{@paper.name} was successfully added to your cart")
+        expect(current_path).to eq("/items")
+
+        within '.topnav' do
+          expect(page).to have_content("Cart: 1")
+        end
+
+        visit "/items/#{@pencil.id}"
+        click_on "Add To Cart"
+
+        within 'nav' do
+          expect(page).to have_content("Cart: 2")
+        end
+      end
+
+      it "can update order quantities in cart" do
+        visit "/items/#{@quantity_test_object_visitor.id}"
+        click_on "Add To Cart"
+
+        visit "/cart"
+        expect(page).to have_button("plus")
+
+        click_button("plus")
+        expect(page).to have_content("You have updated a quantity in your cart")
+
+        click_button("plus")
+        expect(page).to have_content("There are limited supplies in inventory - this item has reached it's max! Please try again later.")
+      end
     end
 
-    it "I can add this item to my cart" do
-      visit "/items/#{@paper.id}"
-      click_on "Add To Cart"
+    describe "As a Regular User" do
+      # visit items, add item, visit cart, increase count, restrictions: no more than inventory allows(model method?)
+    end
 
-      expect(page).to have_content("#{@paper.name} was successfully added to your cart")
-      expect(current_path).to eq("/items")
+    describe "As a Merchant User" do
+      # visit items, add item, visit cart, increase count, restrictions: no more than inventory allows(model method?)
+    end
 
-      within '.topnav' do
-        expect(page).to have_content("Cart: 1")
-      end
-
-      visit "/items/#{@pencil.id}"
-      click_on "Add To Cart"
-
-      within 'nav' do
-        expect(page).to have_content("Cart: 2")
-      end
+    describe "As an Admin User" do
+      # Admins show NOT be able to order items
     end
   end
 end
