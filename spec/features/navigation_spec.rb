c0afc4429e1f6d1a6200e001d706b207ddb97bfb
Priya Power <priyajag72@gmail.com>
2020-09-12 09:42:13 -0600

    Add initial work for us7 test

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -3,6 +3,20 @@ require 'rails_helper'
 
 RSpec.describe 'Site Navigation' do
   describe 'As a Visitor' do
+    before :each do
+      @user = User.create(name:"Jackie Chan", address:"skdjfhdskjfh", city:"kajshd", state:"jsdh", zip:"88888", email: "tombroke@gmail.com", password:"Iamapassword", password_confirmation:"Iamapassword", role: 0)
+
+      visit '/merchants'
+
+      within 'nav' do
+        click_link "Login"
+      end
+
+      fill_in :email, with: @user.email
+      fill_in  :password, with: @user.password
+      click_button "Login"
+    end
+
     it "I see a nav bar with links to all pages" do
       visit '/merchants'
 
@@ -31,7 +45,20 @@ RSpec.describe 'Site Navigation' do
       within 'nav' do
         expect(page).to have_content("Cart: 0")
       end
+    end
+
+    # =========================================================
+    # US7
+    it "can redirect a user to 404 error if they try to access any path with /merchant" do
+      visit "/merchants"
+      # UH OH.... This might be a paired decision - I think that even the landing page /merchants should be avoided by a typical user?
+      # This means that we need to go check all specs, and any visit to "/merchants" needs to be checked it required update to "/"
+      # Then I can find the logic for a before and render when visiting any namespace :merchants
+    end
+
+    it "can redirect a user to 404 error if they try to access any path with /admin" do
 
     end
+    # =========================================================
   end
 end
