181aad25b482ba4f9ec3c722d911d1841d67fdb8
Leah Riffell <leah.riffell@gmail.com>
2020-09-11 21:12:40 -0600

    Remove extra lines

--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -4,9 +4,7 @@ RSpec.describe "Login Page", type: :feature do
   describe "As a visitor" do
     before :each do
       @user = User.create(name:"Luke Hunter James-Erickson", address:"skdjfhdskjfh", city:"kajshd", state:"jsdh", zip:"88888", email: "tombroke@gmail.com", password:"Iamapassword", password_confirmation:"Iamapassword", role: 0)
-
       @merchant = User.create(name:"Leah", address:"123 Sesame Street", city:"New York", state:"NY", zip:"90210", email: "Leahsocool@gmail.com", password:"Imeanit", password_confirmation:"Imeanit", role: 1)
-
       @admin = User.create(name:"Priya", address:"13 Elm Street", city:"Denver", state:"CO", zip:"66666", email: "priyavcooltoo@gmail.com", password:"yuuuuuup", password_confirmation:"yuuuuuup", role: 2)
 
       visit "/merchants"
