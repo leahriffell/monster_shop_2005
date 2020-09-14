84fd9f93a5b31d07ee54a753f32bc6cc8b303e47
Priya Power <priyajag72@gmail.com>
2020-09-14 08:19:05 -0600

    Update cart controller #add with refactor from cart model; update #update_quantity with flash success and failure

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,9 +1,11 @@
 class CartController < Cart::BaseController
+
+
   def add_item
     item = Item.find(params[:item_id])
     cart.add_item(item.id.to_s)
     if cart.contents[item.id] == 0
-      cart.contents.clear
+      cart.empty_cart
       flash[:failure] = "There are limited supplies in inventory - this item has reached it's max! Please try again later."
     else
       flash[:success] = "#{item.name} was successfully added to your cart"
@@ -11,14 +13,23 @@ class CartController < Cart::BaseController
     end
   end
 
+  def update_quantity
+    item = Item.find(params[:item_id])
+    temp_inventory = cart.contents[item.id.to_s]
+    cart.update_item(item.id.to_s)
+    if cart.contents[item.id.to_s] > temp_inventory
+      flash[:success] = "You have updated a quantity in your cart"
+      redirect_to request.referrer
+    else
+      flash[:failure] = "There are limited supplies in inventory - this item has reached it's max! Please try again later."
+      redirect_to request.referrer
+    end
+  end
+
   def show
     @items = cart.items
   end
 
-  def update_quantity
-    item = Item.find(params[:item_id])
-    binding.pry
-  end
 
   def empty
     session.delete(:cart)
