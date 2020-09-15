52878919dd75afd9f013243b0b0e3b2d43b628e7
Priya Power <priyajag72@gmail.com>
2020-09-15 10:30:50 -0600

    Update order database for default attribute status equal false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_150522) do
+ActiveRecord::Schema.define(version: 2020_09_15_162546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_09_15_150522) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.boolean "status?", default: false
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
