7025ee352ea14909d876939000e13bbb23fa6431
Leah Riffell <leah.riffell@gmail.com>
2020-09-14 18:17:49 -0600

    Remove unnecessary test data from index spec

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -3,19 +3,6 @@ require 'rails_helper'
 RSpec.describe "Items Index Page" do
   describe "When I visit the items index page" do
     before(:each) do
-      # @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      # @brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
-
-      # @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-
-      # @pull_toy = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
-      # @dog_bone = @brian.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
-
-      # visit '/items'
-
-      
-
-
       @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @pet_shop = Merchant.create(name: "Ruffhouse", address: '11 Paw Print Lane', city: 'Denver', state: 'CO', zip: 80202)
 
@@ -112,7 +99,6 @@ RSpec.describe "Items Index Page" do
     end
       
     it "can see a section with least popular items and their qty purchased" do 
-      save_and_open_page
       within ".least-popular" do
         expect(page).to have_link(@item_2.name)
         expect(page).to have_link(@item_3.name)
