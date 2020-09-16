03daed15c7094e3305f7b21a21eff4e07ee03dcb
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-16 13:30:57 -0600

    Add logic US34 - allow @merchant to hold Merchant info on index page

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -2,6 +2,7 @@ class Merchant::DashboardController < Merchant::BaseController
   before_action :require_merchant
 
   def index
+    @merchant = Merchant.find(current_user.merchant_id)
   end
 
 end
