36a60475c815756dc28589ae2b9071531204d716
Grant Dempsey <34411972+GDemps@users.noreply.github.com>
2020-09-17 17:22:01 -0600

    Delete base_controller.rb

--- app/controllers/cart/base_controller.rb
+++
@@ -1,9 +0,0 @@
-class Cart::BaseController < ApplicationController
-  before_action :require_not_admin
-
-  def require_not_admin
-    if current_admin?
-      render file: "/public/404"
-    end
-  end
-end
