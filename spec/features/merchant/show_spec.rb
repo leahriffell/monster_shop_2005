71a39f307b3d5f1b48b2e4c5f1ba6c272afc4109
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-16 19:01:02 -0600

    Update test for US 35 - See list of pending orders on merch dashboard

--- spec/features/merchant/show_spec.rb
+++ spec/features/merchant/show_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe 'Site Navigation' do
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@mikes_employee)
 
-
       @item_1 = @bike_shop.items.create!(name: "Bike pump", description: "XYZ", price: 20, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 30)
       @item_2 = @bike_shop.items.create!(name: "Tready tire", description: "XYZ", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 20)
       @item_3 = @bike_shop.items.create!(name: "Shoe clips", description: "XYZ", price: 50, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 10)
@@ -28,20 +27,14 @@ RSpec.describe 'Site Navigation' do
       @order_2 = @user.orders.create!(name: "Susie", address: "12 Sunshine Road", city: "LA", state: "CA", zip: 55555)
       @order_3 = @user.orders.create!(name: "Larry David", address: "555 Palm Dr", city: "LA", state: "CA", zip: 55555)
       @item_order_1 = ItemOrder.create!(order: @order_1, item: @item_1, price: @item_1.price, quantity: 10)
-      @item_order_1 = ItemOrder.create!(order: @order_2, item: @item_1, price: @item_1.price, quantity: 500)
-      @item_order_2 = ItemOrder.create!(order: @order_1, item: @item_2, price: @item_2.price, quantity: 20)
       @item_order_3 = ItemOrder.create!(order: @order_2, item: @item_3, price: @item_3.price, quantity: 30)
-      @item_order_4 = ItemOrder.create!(order: @order_2, item: @item_4, price: @item_4.price, quantity: 40)
-      @item_order_5 = ItemOrder.create!(order: @order_1, item: @item_5, price: @item_5.price, quantity: 50)
       @item_order_6 = ItemOrder.create!(order: @order_3, item: @item_6, price: @item_6.price, quantity: 60)
       @item_order_7 = ItemOrder.create!(order: @order_2, item: @item_7, price: @item_7.price, quantity: 70)
       @item_order_8 = ItemOrder.create!(order: @order_3, item: @item_8, price: @item_8.price, quantity: 80)
       @item_order_9 = ItemOrder.create!(order: @order_2, item: @item_9, price: @item_9.price, quantity: 90)
       @item_order_10 = ItemOrder.create!(order: @order_3, item: @item_10, price: @item_10.price, quantity: 100)
-      @item_order_11 = ItemOrder.create!(order: @order_3, item: @item_12, price: @item_10.price, quantity: 61)
       @item_order_12 = ItemOrder.create!(order: @order_3, item: @item_11, price: @item_10.price, quantity: 600)
 
-      require "pry"; binding.pry
       visit "/merchant"
     end
 
@@ -53,11 +46,13 @@ RSpec.describe 'Site Navigation' do
       expect(page).to have_content(@bike_shop.zip)
     end
 
-    it "if an employee logs in accidentally as a regular user, they cannot see the merchant dashboard, and must log back in as a merchant"
-
     it "can see a list of pending orders" do
+      expect(page).to have_content(@order_1.id)
+      expect(page).to have_content(@order_1.created_at)
+      expect(page).to have_content(@item_order_1.quantity)
+      expect(page).to have_content(@item_order_1.quantity * @item_order_1.price)
 
-
+      save_and_open_page
     end
 
   end
