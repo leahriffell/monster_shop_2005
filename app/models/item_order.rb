7046eb1acd9f8b0699304de07a33e992de010219
Leah Riffell <leah.riffell@gmail.com>
2020-09-17 13:36:37 -0600

    Add validation to item orders model and spec

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,5 +1,7 @@
 class ItemOrder <ApplicationRecord
-  validates_presence_of :item_id, :order_id, :price, :quantity, :fulfilled?
+  validates_presence_of :item_id, :order_id, :price, :quantity
+
+  validates_inclusion_of :fulfilled?, :in => [true, false]
 
   belongs_to :item
   belongs_to :order
