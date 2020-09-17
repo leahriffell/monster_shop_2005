906304d78ea59c28da5ef0a8c79e2bfe2c2d47ec
Leah Riffell <leah.riffell@gmail.com>
2020-09-17 15:24:28 -0600

    Add test for cancelling an order

--- spec/features/orders/show_spec.rb
+++ spec/features/orders/show_spec.rb
@@ -7,8 +7,11 @@ RSpec.describe "order show page", type: :feature do
 
       @bike_shop = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @tire = @bike_shop.items.create!(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      @pull_toy = @bike_shop.items.create!(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+
       @order_1 = @user.orders.create!(name: "Tommy boy", address: "1234 Street", city: "Metropolis", state: "CO", zip: 12345)
       @item_order_1 = ItemOrder.create!(order: @order_1, item: @tire, price: @tire.price, quantity: 10)
+      @item_order_2 = ItemOrder.create!(order: @order_1, item: @pull_toy, price: @pull_toy.price, quantity: 3, fulfilled?: true)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     end
@@ -31,26 +34,27 @@ RSpec.describe "order show page", type: :feature do
       end
 
       expect(page).to have_content("Total item quantity: #{@order_1.total_quantity}")
-      expect(page).to have_content("Total: $1,000.00")
+      expect(page).to have_content("Total: $1,030.00")
     end
 
     it "can cancel an order" do 
       visit "profile/orders/#{@order_1.id}"
 
-      # click_button "Cancel Order" 
+      click_button "Cancel Order"
 
+      @order_1.item_orders.all?{ |item_order| expect(item_order.fulfilled?).to eq(false) }
+      # expect(@order_1.status).to eq("Cancelled")
+
+      # part about qty decrease
+
+      expect(current_path).to eq("/profile")
+      expect(page).to have_content("Your order has been cancelled")
+
+      visit "/profile/orders" 
+
+      within ("#order-#{@order_1.id}") do 
+        expect(page).to have_content("Status: Cancelled")
+      end
     end
   end
 end
-
-# As a registered user
-# When I visit an order's show page
-# I see a button or link to cancel the order
-# When I click the cancel button for an order, the following happens:
-
-# Each row in the "order items" table is given a status of "unfulfilled"
-# The order itself is given a status of "cancelled"
-# Any item quantities in the order that were previously fulfilled have their quantities returned to their respective merchant's inventory for that item.
-# I am returned to my profile page
-# I see a flash message telling me the order is now cancelled
-# And I see that this order now has an updated status of "cancelled"
