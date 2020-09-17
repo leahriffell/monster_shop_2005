533fe34f060143af308fe2d29fdc9e789dc84364
Grant Dempsey <34411972+GDemps@users.noreply.github.com>
2020-09-17 17:22:40 -0600

    Delete base_controller.rb

--- app/controllers/merchants/base_controller.rb
+++
@@ -1,9 +0,0 @@
-class Merchants::BaseController < ApplicationController
-  before_action :require_merchant
-
-  def require_merchant
-    if !current_merchant?  
-      render file: "/public/404"
-    end
-  end
-end
